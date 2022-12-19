51643a0e8207510bd773460594a07b40dbbfa137
diegofpp <difepor@gmail.com>
2022-12-19 18:55:51 -0300

    creacion de estilos y variables js

---
+++ app.js
@@ -0,0 +1,4 @@
+const dropArea = document.querySelector(".drop-area");
+const dragText = dropArea.querySelector("h2");
+const button = dropArea.querySelector("button");
+const input = dropArea.querySelector("#input-file");
