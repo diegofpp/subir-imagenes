f2cf0676df635c0f0bcab7abc5c68c0695cdf772
diegofpp <difepor@gmail.com>
2022-12-19 21:45:37 -0300

    creacion de evento click en el input creacion de eventos para soltar y arrastrar archivos

--- app.js
+++ app.js
@@ -2,3 +2,59 @@ const dropArea = document.querySelector(".drop-area");
 const dragText = dropArea.querySelector("h2");
 const button = dropArea.querySelector("button");
 const input = dropArea.querySelector("#input-file");
+let files;
+
+button.addEventListener("click", (e) => {
+    input.click();
+});
+
+input.addEventListener("change", (e) => {
+    files = this.files;
+    dropArea.classList.add("active");
+    showFiles(files);
+    dropArea.classList.remove("active");
+});
+
+dropArea.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    dropArea.classList.add("active");
+    dragText.textContent = "Suelta para subir las imagenes";
+});
+
+dropArea.addEventListener("dragleave", (e) => {
+    e.preventDefault();
+    dropArea.classList.remove("active");
+    dragText.textContent = "Arrastra y suelta imagenes";
+});
+
+dropArea.addEventListener("drop", (e) => {
+    e.preventDefault();
+    files = e.dataTransfer.files;
+    showFiles(files);
+    dropArea.classList.remove("active");
+    dragText.textContent = "Arrastra y suelta imagenes";
+});
+
+
+function showFiles(files){
+    if(files.length === undefined){
+        processFile(files);
+    }else{
+        for(const file of files){
+            processFile(file);
+        }
+    }
+
+}
+
+function processFile(file){
+    const docType = file.type;
+    const validExtensions = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'];
+
+    if(validExtensions.includes(docType)){
+        
+    }else{
+      alert("No es un archivo valido");  
+    }
+}
+
